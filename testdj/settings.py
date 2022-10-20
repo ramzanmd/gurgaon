7bea310b9bcefdf646df381668f092977b1c1606
Md Ramzan <ramzanmd865@gmail.com>
2022-10-20 16:58:54 +0530

    first commit

--- testdj/settings.py
+++ testdj/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = ['django-insecure-!nx_b^f(*g-3ip&3i#&ss-=7*ci3u4w07a81q*xg1zolx%)oft']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
